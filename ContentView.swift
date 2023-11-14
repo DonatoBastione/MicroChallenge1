515254c6a6e403d4c36c9f2cd6b235b2e342c313
DonatoBastione <dbastione23@fed.idserve.net>
2023-11-14 10:26:17 +0100

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MicroChallenge1
+//
+//  Created by DONATO BASTIONE on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MicroChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  MicroChallenge1App.swift
+//  MicroChallenge1
+//
+//  Created by DONATO BASTIONE on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MicroChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
